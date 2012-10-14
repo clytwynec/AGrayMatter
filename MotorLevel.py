1197700e54ea7e07a80e31d00bde809ff20d9637
clytwynec <chris.lytwynec@gmail.com>
2012-10-13 21:39:17 -0400

    motor level

--- MotorLevel.py
+++ MotorLevel.py
@@ -13,6 +13,10 @@ class MotorLevel(Level):
 		self.mCraneSwitch = None
 		self.mDoor = None
 		self.mCraneBox = None
+		self.mDoorTriggered = 0
+
+		self.mMotorWire, self.mMotorRect = self.mKernel.ImageManager().LoadImage('motor_wire.bmp')
+
 
 	def Load(self, levelName):
 		Level.Load(self, levelName)
@@ -21,10 +25,22 @@ class MotorLevel(Level):
 		self.mDoor = MotorDoor(self.mKernel, self)
 		self.mCraneBox = CraneBox(self.mKernel, self)
 
+
 		self.mCraneSwitch.SetPosition([348,27])
-		self.mDoor.SetPosition([350,375])
+		self.mDoor.SetPosition([350,700])
 		self.mCraneBox.SetPosition([675,325])
 
 		self.mEntities.append(self.mCraneSwitch)
 		self.mEntities.append(self.mDoor)
 		self.mEntities.append(self.mCraneBox)
+
+
+	def Draw(self):
+		Level.Draw(self)
+		self.mLevelSurface.blit(self.mMotorWire, self.mMotorRect)
+
+	def Update(self, delta):
+		Level.Update(self, delta)
+
+		if(self.mDoorTriggered >= 2):
+			self.mDoor.LiftDoor()
